815990807f04dd79897ce8e691cdc0e9df75f071
brunobuddy <bruno@buddyweb.fr>
2021-04-29 18:46:14 +0200

    fix(filter): interface not strict enough and causes errors

--- projects/abacus-angular-library/src/lib/interfaces/filter.interface.ts
+++ projects/abacus-angular-library/src/lib/interfaces/filter.interface.ts
@@ -2,15 +2,15 @@ import { InputType } from '../enums/input-type.enum'
 import { SelectOption } from './select-option.interface'
 
 export interface Filter {
-  label?: string
+  label: string
+  properties: { [key: string]: string }
   placeholder?: string
-  properties?: { [key: string]: string }
   initialValue?: { [key: string]: string }
   inputType: InputType
   className?: string
   async?: boolean
-  selectOptions?: SelectOption[] | (() => Promise<SelectOption[]>)
 
   // Input-specific props
   searchResources?: string[]
+  selectOptions?: SelectOption[] | (() => Promise<SelectOption[]>)
 }
