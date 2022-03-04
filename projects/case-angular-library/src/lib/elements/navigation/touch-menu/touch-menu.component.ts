5e364109a6108e1408fe26d0559d73231f8b9d40
Sébastien Conejo <sebastien.conejo@buddyweb.fr>
2022-03-04 16:49:08 +0100

    fix: menu was broken and toggle didn't work Closes agencebuddyweb/neorhea#91

--- projects/case-angular-library/src/lib/elements/navigation/touch-menu/touch-menu.component.ts
+++ projects/case-angular-library/src/lib/elements/navigation/touch-menu/touch-menu.component.ts
@@ -7,7 +7,7 @@ import {
   Renderer2,
   ViewChild
 } from '@angular/core'
-import { Power2, TweenLite } from 'gsap'
+import { gsap, Power2 } from 'gsap'
 
 import { CaseConfig } from '../../../interfaces/case-config.interface'
 import { MenuItem } from '../../../interfaces/menu-item.interface'
@@ -74,34 +74,40 @@ export class TouchMenuComponent implements OnInit {
 
   triggerMenuAnimation() {
     if (this.showMenu) {
-      TweenLite.to(this.menuEl.nativeElement, 0.01, {
-        className: '+=is-open'
+      gsap.to(this.menuEl.nativeElement, {
+        className: '+=is-open',
+        duration: 0.01
       })
 
-      TweenLite.to(this.menuItemsEl.nativeElement, 0.5, {
+      gsap.to(this.menuItemsEl.nativeElement, {
         opacity: 1,
         left: '0',
-        ease: Power2.easeInOut
+        ease: Power2.easeInOut,
+        duration: 0.5
       })
-      TweenLite.to(this.hidingLayerEl.nativeElement, 0.5, {
+      gsap.to(this.hidingLayerEl.nativeElement, {
         autoAlpha: 0.9,
-        ease: Power2.easeInOut
+        ease: Power2.easeInOut,
+        duration: 0.5
       })
       this.renderer.addClass(document.querySelector('html'), 'is-clipped')
     } else {
-      TweenLite.to(this.menuEl.nativeElement, 0.01, {
-        className: '-=is-open'
+      gsap.to(this.menuEl.nativeElement, {
+        className: '-=is-open',
+        duration: 0.5
       })
 
-      TweenLite.to(this.menuItemsEl.nativeElement, 0.5, {
+      gsap.to(this.menuItemsEl.nativeElement, {
         opacity: 0.8,
         left: '-100%',
         ease: Power2.easeInOut,
+        duration: 0.5,
         delay: 0.1
       })
-      TweenLite.to(this.hidingLayerEl.nativeElement, 0.5, {
+      gsap.to(this.hidingLayerEl.nativeElement, {
         autoAlpha: 0,
-        ease: Power2.easeInOut
+        ease: Power2.easeInOut,
+        duration: 0.5
       })
       this.renderer.removeClass(document.querySelector('html'), 'is-clipped')
     }
