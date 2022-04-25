9d28ae94fbc1738bc2b82e949132fa8d97725c01
brunobuddy <bruno@buddyweb.fr>
2022-04-25 15:34:31 +0200

    feat(date): 2 digits for day number

--- projects/case-angular-library/src/lib/elements/yields/date-yield/date-yield.component.ts
+++ projects/case-angular-library/src/lib/elements/yields/date-yield/date-yield.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core'
 
 @Component({
   selector: 'case-date-yield',
-  template: ` {{ date | date: 'd/MM/yy' }} `,
+  template: ` {{ date | date: 'dd/MM/yy' }} `,
   styleUrls: ['./date-yield.component.scss']
 })
 export class DateYieldComponent {
